433625b3256b685a4031426bf2c6b1d8a6f53ed3
Yuliang Xiao <41984619+unravel11@users.noreply.github.com>
2023-10-20 02:30:08 +0000

    sort pdfList

--- genPdfJson.py
+++ genPdfJson.py
@@ -8,6 +8,8 @@ def generate_pdf_list(directory="pdf"):
         for file in files:
             if file.endswith('.pdf'):
                 pdf_list.append({"category": category, "name": file})
+    ##根据pdf文件名排序，文件名为XX_数字_中文名.pdf,根据数字大小排序
+    pdf_list = sorted(pdf_list, key=lambda x: int(x['name'].split('_')[1]))
 
     with open('pdfList.json', 'w') as f:
         json.dump(pdf_list, f, indent=4)
